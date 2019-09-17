8f35407b9c3a1e242ffbf975e0a312e442220ed6
Don Pato <patodev@gmail.com>
2019-09-17 11:22:05 +0200

    adds new components

--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ArticlesRoutingModule } from './articles-routing.module';
+import { ArticlesComponent } from './articles/articles.component';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ArticlesComponent, ArticleDetailsComponent],
   imports: [
     CommonModule,
     ArticlesRoutingModule
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { ProductsComponent } from './products/products.component';
+import { ProductsDetailsComponent } from './products-details/products-details.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductsComponent, ProductsDetailsComponent],
   imports: [
     CommonModule,
     ProductsRoutingModule
