975a36ee772ac270fbdedadf2efc18a5c04e2d56
Don Pato <patodev@gmail.com>
2019-09-17 12:43:18 +0200

    An alternative way, we can navigate from articles to article details view with params programmatically using Typescript code

--- src/app/articles/articles/articles.component.ts
+++ src/app/articles/articles/articles.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-articles',
-  templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.scss']
+    selector: 'app-articles',
+    templateUrl: './articles.component.html',
+    styleUrls: ['./articles.component.scss']
 })
 export class ArticlesComponent implements OnInit {
 
-  constructor() { }
+    constructor(private router: Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  article = {
-    id: 100,
-    title: 'How to make router & navigation in Angular 8',
-    author: 'Didin J.',
-    description: 'A complete tutorial about creating router and navigation in the Angular 8 Web Application'
-  };
+    article = {
+        id: 100,
+        title: 'How to make router & navigation in Angular 8',
+        author: 'Didin J.',
+        description: 'A complete tutorial about creating router and navigation in the Angular 8 Web Application'
+    };
+
+    gotoDetails(articleId: any) {
+        this.router.navigate(['/article/', articleId]);
+    }
 
 }
